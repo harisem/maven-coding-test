9c0a740be6f93b5f5eb9de1f4e4c62c401022ceb
harisem <harmuhammadis@gmail.com>
2022-06-11 16:51:22 +0700

    validasi

--- pecahan.js
+++ pecahan.js
@@ -1,12 +1,18 @@
-var output = []
 var btnSubmit = document.getElementById('submit')
 btnSubmit.addEventListener('click', generate)
 
 function generate() {
+    var output = []
+    document.getElementById('myTable').innerHTML = ''
+
     var number = document.getElementById('number').value
     var sNumber = number.toString()
 
     for (var i = 0, len = sNumber.length; i < len; i++) {
+        if (+sNumber.charAt(i) === 0) {
+            output = ['Tidak boleh ada angka 0']
+            break
+        }
         var length = len - 1
         var n = Math.pow(10, length - i)
         output.push(+sNumber.charAt(i) * n)
